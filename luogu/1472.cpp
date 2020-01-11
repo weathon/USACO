07684f7b2c3f2b51434306c488dba86b95731b84
wguo6358 <wguo6358@gmail.com>
2020-01-11 12:14:37 -0600

    OneNOte New Note #1

---
+++ luogu/1472.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+const int Mod = 9901;
+int n, k;
+int f[210][210];
+
+int main()
+{
+    cin >> n >> k;
+    // for (int i = 0; i < 210; i++)
+    // {
+    //     f[0][i] = 1;
+    //     f[1][i] =1;//???Will not affect because the n of this is not enough?
+    // }
+    f[1][1] = 1;                    //Still that questions, do we need to fill in all them 1?(The first column?)
+    for (int i = 3; i <= n; i += 2) //The start point  was i=3?
+    {
+        for (int j = 2; j <= k; j++) //num of   i is from 3
+        {
+            for (int myleft = 1; myleft < i; myleft += 2) //At least at myleft, but this n is not the root problem's n, also need -1 (Itself)
+            {                                             //Cannot myleft<=i, if "=" then it self is 0?
+                // int myright=n-myleft-1;//not n, because it is not set yet
+                int myright = i - myleft - 1; //Father problem's value ans sub-
+                //Num of ways, not the max!
+                //All with depth x-1
+                f[i][j] += (f[myleft][j - 1] * f[myright][j - 1]) % Mod;
+                //myleft is x-1, y is anything
+                if (myleft != myright)
+                {
+                    int mymax = 0;
+                    for (int o = 0; o <j; o++) //Just take the last one?  Not <=j
+                    {
+                        if (f[myright][o] > mymax)
+                            mymax = f[myright][o];
+                    }
+                    f[i][j] += (f[myleft][j - 1] * mymax) % Mod;
+
+                    //The other way
+                    mymax = 0;
+                    for (int o = 0; o < j ; o++) //Just take the last one?
+                    {
+                        // if(f[myleft][o]>mymax) mymax=f[myright][o];
+                        if (f[myleft][o] > mymax)
+                            mymax = f[myleft][o];
+                    }
+                    // f[i][j]+=(f[myleft][j-1]*mymax)%Mod;
+                    f[i][j] += (f[myright][j - 1] * mymax) % Mod;
+                }
+            }
+        }
+    }
+    //Debug, at last var?
+    cout << f[n][k] - f[n][k-1]<< endl; //By debug, f[n][k] and f[n][k-1] is both 2, that makes it 0
+    return 0;
+}
