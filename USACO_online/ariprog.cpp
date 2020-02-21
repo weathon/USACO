c39aa5f3e13387241aaa5e177f80c79d2c3ae7d9
wguo6358 <wguo6358@gmail.com>
2020-02-21 14:15:41 -0600

    常数优化好难啊啊啊啊啊啊啊啊啊啊

--- USACO_online/ariprog.cpp
+++ USACO_online/ariprog.cpp
@@ -61,25 +61,26 @@ using namespace std;
 
 int N;
 int maxpq;
-// int bisquares[100000];
+int bisquares[5000000];
 short tong[225000];
 int pointer = 0;
 
 int main(void)
 {
-    freopen("ariprog.in", "r", stdin);
-    freopen("ariprog.out", "w", stdout);
+    // freopen("ariprog.in", "r", stdin);
+    // freopen("ariprog.out", "w", stdout);
     //看了别人用人用sort做
     //上升序列？
-    cin >> N;
-    cin >> maxpq;
+    // cin >> N;
+    // cin >> maxpq;
+    scanf("%d%d",&N,&maxpq);
     int q; //常优
     for (int p = 0; p <= maxpq; p++)
     {
         for (q = 0; q <= maxpq; q++)
         {
-            // bisquares[pointer] = p * p + q * q;
-            tong[p * p + q * q] = 1;
+            bisquares[pointer] = p * p + q * q;
+            tong[bisquares[pointer]] = 1;
             pointer++;
         }
     }
@@ -102,14 +103,16 @@ int main(void)
     for (b = 1; b < (maxppqq*2)/(N-2); b++)//优化maxppqq为maxdiffer?不行，可以跨越的  这里可以N-2,智商刚才和下面的一起修改了，瞬间快了好多，最后的结束没有了
     {
         nb_1=b*(N-1);//这个放在外面最后一个点过了
-        for (a = 0; a < maxppqq; a++)//直接用整除取余数可以吗  一倍就够了，很快了
+        // for (j = 0; j < pointer; j++)//直接用整除取余数可以吗  一倍就够了，很快了
+        for(int a=0;a<maxppqq;a++)
         {
+            // a=bisquares[j];//直接扫描的话快乐很多，但是一堆WA
+            // cout<<a<<endl;
             // if(a+b*(N-1)>maxppqq2*2) continue;//改成N-1就可以AC了，但是是有两个点超时
             if(a+nb_1>maxppqq2) break;//改成N-1就可以AC了，但是是有两个点超时
 
-            // a=bisquares[j];
             //如果是先a可以jianzhi?
-            // if(tong[a]!=1) continue;
+            if(tong[a]!=1) continue;//加上不超时，不是这一行，看109，但是错了很多
             //https://www.luogu.com.cn/problemnew/solution/P1214?page=3
             //可以直接扫描数组 bisquares
             current = a - b;
