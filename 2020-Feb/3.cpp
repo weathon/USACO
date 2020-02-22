66155304bba1d4f002f91d71e53b27883a9293ee
weathon <wguo6358@gmail.com>
2020-02-22 12:03:59 -0600

    backup

--- 2020-Feb/3.cpp
+++ 2020-Feb/3.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int N, K, A1, A2, B1, B2;
 
 int numlist[200];
-int tmplist[200]; //不直接删除，而是标记？
+int tmplista[200]; //不直接删除，而是标记？
 //有机会过
+int tmplistb[200]; //不直接删除，而是标记？
 
 int main()
 {
@@ -19,30 +20,29 @@ int main()
     }
     for (int j = 0; j < K; j++)
     {
-        // int tmplist[200]; //不直接删除，而是标记
-        for (int _ = 0; _ < 200; _++)
-            tmplist[_] = -1;
-        for (int a = A1; a <= A2; a++)
+        // cout<<B1;
+        for (int a = 1; a <= (A2 - A1) / 2; a++)
         {
-            // tmplist[a] = numlist[A1 - a + 1];
-            tmplist[a-1] = numlist[A2 - a]; //2 but not 1? bujianbujian
-        }
-        for (int a = A1; a <= A2; a++)
-        {
-            numlist[a] = tmplist[a-1]; //还是下标01问题  早解决tmp下biaojiuhao;
-        }
-        /////////////////////////////
-        for (int _ = 0; _ < 200; _++)
-            tmplist[_] = -1;
 
-        for (int b = B1; b <= B2; b++) //格式化快捷键不好用 喝太多水
-        {
-            tmplist[b-1] = numlist[B2 - b];
+            // int tmp;
+            // tmp = numlist[A2 - a + 1];
+            // numlist[A2 - a + 1] = numlist[a];
+            // numlist[a] = tmp;//交换两次了？  这里debug,明明tmp是3，到数组里面变成2?
+            // // cout<<numlist[a]<<" "<<tmp<<endl;
+            swap(numlist[A2 - a + 1], numlist[a]);
         }
-        for (int b = B1; b <= B2; b++)
+
+        for (int b = 1; b <= (B2 - B1) / 2; b++) //格式化快捷键不好用 喝太多水
         {
-            numlist[b] = tmplist[b-1]; //-1, not +1?  不是numlistjian,ershi bi ji caogao
+            //     int tmp;
+            //     tmp = numlist[B2 - b + 1];//B2还是N?
+            // //    cout<<tmp<<" "<<numlist[b]<<" "<<numlist[B2 - b + 1]<<endl;
+            //     numlist[B2 - b + 1] = numlist[b];
+            //     numlist[b] = tmp;
+            //    cout<<tmp<<" "<<numlist[b]<<" "<<numlist[B2 - b + 1]<<endl;
+            swap(numlist[B2 - b + 1], numlist[b]);
         }
+
     } //分开都没问题，合到一起就不行了？ 局部？
     for (int i = 1; i <= N; i++)
     {
