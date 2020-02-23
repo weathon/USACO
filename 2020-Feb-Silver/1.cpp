f72a1e761684125848d5d3bf4ac9d3cf2476b501
weathon <wguo6358@gmail.com>
2020-02-22 18:25:51 -0600

    backu

--- 2020-Feb-Silver/1.cpp
+++ 2020-Feb-Silver/1.cpp
@@ -1,69 +1,83 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 using namespace std;
 
 int N, M, K;
 int Ls[200];
 int Rs[200];
-int cows[200000];
+int cows[3][200000];
 int ref[200000];
 int L, R;
 int tmp;
+// int ansmemory[]
+//回来的路上突然想到 因为每一次M都是一样的，先扫描一次变为指定函数
+int map[200000];
 
-bool ifsame(int A[200], int B[200])
-{
-    for (int i = 1; i <= N; i++)
-        if (A[i] != B[i])
-            return false;
-    return true;
-}
-
+int times;
 int main()
 {
     freopen("swap.in", "r", stdin);
     freopen("swap.out", "w", stdout);
+    // ios_base::sync_with_stdio(false); cin.tie(0);  //加上这一行答案就不正确了
     cin >> N >> M >> K;
     for (int i = 1; i <= M; i++)
     {
-        cin >> Ls[i] >> Rs[i];
+        // cin >> Ls[i] >> Rs[i];
+        scanf("%d%d", &Ls[i], &Rs[i]);
     }
     for (int i = 1; i <= N; i++)
     {
-        cows[i] = i;
-        ref[i]=i;
+        cows[0][i] = i;
+        map[i] = i;
     }
-    int samema = -1;
-    for (int times = 1; times <= K; times++)
+    int j;
+    for (int i = 1; i <= M; i++)
     {
-        if(ifsame(ref,cows) and times!=1){
-            samema=times-1;
-        }
-        for (int i = 1; i <= M; i++)
+        // L = Ls[i];
+        // R = Rs[i];
+        // cout<<L<<" "<<R<<endl;  2 5 3 7 两次
+        for (j = Ls[i]; j <= (Rs[i] + 1 - Ls[i]) / 2 + Ls[i] - 1; j++)
         {
-            L = Ls[i];
-            R = Rs[i];
-            // cout<<L<<" "<<R<<endl;  2 5 3 7 两次
-            for (int j = L; j <= (R + 1 - L) / 2 + L - 1; j++)
-            {
-                //不能用之前的代码
-                // cout<<cows[R-j+L]<<" "<<cows[j]<<endl;  样例输出 5 2\n 2 5
-                // cout<<j<<" "<<R-j+L<<endl;  //两个2 5
-                tmp = cows[R - j + L];
-                cows[R - j + L] = cows[j];
-                cows[j] = tmp; //太吵，写错了
-            }
+            // map[Rs[i] - j + Ls[i]]=cows[0][j];
+            // map[j]=cows[0][Rs[i] - j + Ls[i]]; //必须用cow而不是map？
+            // tmp = map[Rs[i] - j + Ls[i]];
+            // map[Rs[i] - j + Ls[i]] = map[j];
+            // map[j] = tmp; //太吵，写错了
+            swap(map[j], map[Rs[i] - j + Ls[i]]);  //也不行，来回交换了？ 检测来回交换来优化？太累了不想写
 
-            // for (int i = 1; i <= N; i++)
-            // {
-            //     cout << cows[i] << endl;
-            // }
-            // cout << endl;
-            // cout<<endl;
         }
     }
-    for (int i = 1; i <= N; i++)
+
+    // int samema = -1;
+    // aatimes = 0;
+    // int att=0;
+    int i;
+    for (int times = 1; times <= K%(M*K); times++) //tring 2m 2k mk
     {
-        cout << cows[i] << endl;
+        for (i = 1; i <= N; i++)
+        {
+            cows[times % 2][i] = cows[(times - 1) % 2][map[i]];
+        }
+    }
+    // times=0;
+    // if (samema != -1)
+    // {
+    //     for (int times = 1; times <= K % samema; times++)
+    //     {
+    //         for (int i = 0; i <= N; i++)
+    //         {
+    //             cows[times % 2][i] = cows[(times - 1) % 2][map[i]];
+    //         }
+    //         times++;
+    //         att=times;
+    //     }
+    // }
+    // cout<<times<<endl;
+    for (i = 1; i <= N; i++)
+    {
+        // cout << cows[(att) % 2][i] << endl;
+        printf("%d\n", cows[(K % (M * K)) % 2][i]);
     }
     return 0;
 }
--- 2020-Feb-Silver/2.cpp
+++ 2020-Feb-Silver/2.cpp
@@ -1,43 +1,56 @@
 #include <iostream>
 #include <fstream>
+#include<vector>
+#include <map>
+//https://www.cnblogs.com/mr-wid/archive/2013/01/22/2871105.html  https://blog.csdn.net/qq_31918961/article/details/88934815 whole programe
 using namespace std;
 
 int N;
 int X[100005];
 int Y[100005];
-long long ans;
+// int xhash[3001][10000]; //有负数，要注意处理
+map<int, 
+int xhashpointer[2001];
+int yhash[3001][10000]; //有负数，要注意处理
+int yhashpointer[2001];
+// vector<int> a;
+long long myans;
+
 
 void solve(int pointid)
 {
-    for (int i = 0; i < N; i++)
+    int myxxx, myyyy;
+    for (int i = 0; i < xhashpointer[X[pointid]]; i++)
     {
-        //遍历X
-        if (Y[i] != Y[pointid])
-            continue;
-        for (int j = 0; j < N; j++)
+        //所有相同的x,空间换时间
+        myyyy = xhash[X[pointid]][i];
+        for (int j = 0; j < yhashpointer[Y[pointid]]; j++)
         {
-            // 遍历Y
-            if (i == j)
-                continue;
-            if (X[j] != X[pointid])
-                continue;
-            ans+=abs(X[i]-X[pointid])*abs(Y[j]-Y[pointid]);
-            ans=ans%1000000007;//太紧张噪音了
+            myxxx = yhash[Y[pointid]][j];
+            myans += abs(myxxx - X[pointid]) * abs(myyyy - Y[pointid]);
+            if(myans>1000000007) myans=myans%1000000007;
         }
     }
 }
 
 int main()
 {
-    freopen("triangles.in", "r", stdin);
+    // a[100]=0;
+    freopen("triangles.in", "r", stdin); //原来根本没有输入文件 困
     freopen("triangles.out", "w", stdout);
     cin >> N;
     for (int i = 0; i < N; i++)
     {
         cin >> X[i] >> Y[i];
+        X[i]+=1001;
+        Y[i]+=1001;
+        xhash[X[i]][xhashpointer[X[i]]] = Y[i];
+        xhashpointer[X[i]]++;
+        yhash[Y[i]][yhashpointer[Y[i]]] = X[i];
+        yhashpointer[Y[i]]++;
     }
     for (int i = 0; i < N; i++)
         solve(i);
-    cout << ans << endl;
+    cout << myans << endl;
     return 0;
 }
