6257b31c48a5c4087531a980e5ea43e2f2d5d5d8
wguo6358 <wguo6358@gmail.com>
2020-03-04 21:45:17 -0600

    a

---
+++ test_auto_loop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int a[5]={1,2,3,4,5};//[ {写错了
+int main()
+{
+	for(int i:a) cout<<i<<endl;//如果要修改可以用指针
+	//https://blog.csdn.net/FX677588/article/details/70157088
+	return 0;
+}
