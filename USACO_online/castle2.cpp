7a49efd15181bdd550d869849b0d861f29cb1d9f
weathon <wguo6358@gmail.com>
2020-03-07 23:03:58 -0600

    房间数正常了

--- USACO_online/castle2.cpp
+++ USACO_online/castle2.cpp
@@ -32,9 +32,9 @@ int main(void)
     freopen("castle.in", "r", stdin);
     // freopen("castle.out", "w", stdout);
     cin >> m >> n;
-    for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++) //调换了ij没调换mn？
     {
-        for (int j = 0; j < n; j++)
+        for (int i = 0; i < m; i++)
         {
             {
                 cin >> tmp;
@@ -43,22 +43,22 @@ int main(void)
                 myup = 1 & (tmp >> 1);
                 myright = 1 & (tmp >> 2);
                 mydown = 1 & (tmp >> 3); //这里写了right
-                if (!myleft)
+                if (false == myleft)
                 {
                     room[i][j][tmppointer] = 3;
                     tmppointer++;
                 }
-                if (!myright)
+                if (false == myright)
                 {
                     room[i][j][tmppointer] = 4;
                     tmppointer++;
                 }
-                if (!myup)
+                if (false == myup)
                 {
                     room[i][j][tmppointer] = 1;
                     tmppointer++;
                 }
-                if (!mydown)
+                if (false == mydown)
                 {
                     room[i][j][tmppointer] = 2;
                     tmppointer++;
@@ -68,11 +68,16 @@ int main(void)
             // dfs(i, j, roomcounter, 0);
         }
     }
-
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
+    for (int j = 0; j < n; j++)
+        for (int i = 0; i < m; i++)
             dfs(i, j, roomcounter, 0);
     cout << roomcounter << endl;
+    // for (int i = 0; i < m; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //         cout << colorgraph[i][j] << " ";
+    //     cout<<endl;
+    // }
     return 0;
 }
 
@@ -80,17 +85,18 @@ void dfs(int x, int y, int color, int deepth)
 {
     if (colorgraph[x][y] == 0)
     {
+        // cout<<x<<" "<<y<<" "<<pointerlist[x][y]<<endl;  有一个0可能是越界 没有越界检测？
         for (int _ = 0; _ < pointerlist[x][y]; _++)
         {
             colorgraph[x][y] = color;
             if (room[x][y][_] == 1)
-                dfs(x + 1, y, color, deepth + 1);
+                dfs(x, y-1, color, deepth + 1);
             if (room[x][y][_] == 2)
-                dfs(x - 1, y, color, deepth + 1);
+                dfs(x , y+1, color, deepth + 1);
             if (room[x][y][_] == 3)
-                dfs(x, y - 1, color, deepth + 1);
+                dfs(x-1, y, color, deepth + 1);
             if (room[x][y][_] == 4)
-                dfs(x, y - 1, color, deepth + 1);
+                dfs(x+1, y, color, deepth + 1);
         }
         if (deepth == 0)
         {
--- USACO_online/testbit.cpp
+++ USACO_online/testbit.cpp
@@ -1,11 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <cstdio>
 using namespace std;
 
 int tmp;
 
 int main()
 {
-    int a=8,b=8,c;
-    c=8;
+    int tmp = 15;
+    bool myleft = 1 & tmp;
+    bool myup = 1 & (tmp >> 1);
+    bool myright = 1 & (tmp >> 2);
+    bool mydown = 1 & (tmp >> 3); //这里写了right
+    printf("%d %d %d %d",myleft,myright,myup,mydown);
     return 0;
 }
