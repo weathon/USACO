759e6b9f3597587366c08078d00bcecfc824de6c
weathon <wguo6358@gmail.com>
2020-03-09 09:07:37 -0500

    up

---
+++ USACO_online/hamming.cpp
@@ -0,0 +1,20 @@
+/*
+ID: wguo6352
+TASK: hamming
+LANG: C++14                 
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int n,b,d;
+
+int main()
+{
+    freopen("hamming.in", "r", stdin);
+    // freopen("hamming.out", "w", stdout);
+    cin>>n>>b>>d;
+        
+    return 0;
+}
---
+++ USACO_online/test.cpp
@@ -0,0 +1,10 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+
+int a=0;
+int main()
+{
+    cout<<bool(a=0)<<endl;
+    return 0;
+}
