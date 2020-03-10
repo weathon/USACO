c074e3070de349a6cc0654b4a6ea64b81b2b7b64
weathon <wguo6358@gmail.com>
2020-03-09 23:09:11 -0500

    完成空格处理，感觉自己的像BFS?

--- USACO_online/hamming.cpp
+++ USACO_online/hamming.cpp
@@ -17,17 +17,17 @@ int hammingd(int, int);
 vector<int> ans;
 int main()
 {
-    // freopen("hamming.in", "r", stdin);
-    // freopen("hamming.out", "w", stdout);
+    freopen("hamming.in", "r", stdin);
+    freopen("hamming.out", "w", stdout);
     cin >> n >> b >> d;
     // cout<<hammingd(n,d)<<endl;
     ans.push_back(0);
-    int count=0;//init
+    int count = 0; //init
     bool ifans;
     for (int testnum = 1; true; testnum++) //0默认是答案了
     {
         // cout<<count<<endl;
-        if (count >= n-1)//忘了-1,一直死循环？
+        if (count >= n - 1) //忘了-1,一直死循环？
             break;
         ifans = true;
         for (auto num : ans)
@@ -44,8 +44,21 @@ int main()
             ans.push_back(testnum);
         }
     }
+    int i = 0;
+    // int j;
     for (auto num : ans)
-        cout << num << " ";
+    {
+        if (i % 10 != 9) //%9不可行，行数多的hua，这就是有一次下面黏在一起
+        {
+            if(i==n-1) break;
+            cout << num << " ";
+        }
+        else
+   {         if(i==n-1) break;//漏了下面的情况，差点忘了括号
+            cout << num << endl;}
+        i++;
+    }
+    cout << ans[n - 1] << endl;
 }
 
 int tmp;
