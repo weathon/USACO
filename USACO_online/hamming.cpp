e3709f72de31f8e156df6553149625e7473dddad
weathon <wguo6358@gmail.com>
2020-03-09 22:28:46 -0500

    一直输出0

--- USACO_online/hamming.cpp
+++ USACO_online/hamming.cpp
@@ -6,15 +6,69 @@ LANG: C++14
 
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int n,b,d;
+int n, b, d;
+int twoton[20] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
 
+int hammingd(int, int);
+vector<int> ans;
 int main()
 {
-    freopen("hamming.in", "r", stdin);
+    // freopen("hamming.in", "r", stdin);
     // freopen("hamming.out", "w", stdout);
-    cin>>n>>b>>d;
-        
-    return 0;
+    cin >> n >> b >> d;
+    // cout<<hammingd(n,d)<<endl;
+    ans.push_back(0);
+    int count;
+    bool ifans;
+    for (int testnum = 1;true; testnum++) //0默认是答案了
+    {
+        if (count >= n)
+            break;
+        ifans = true;
+        for (auto num : ans)
+        {
+            if (hammingd(num, testnum) < d)
+            {
+                ifans = false;
+                break;
+            }
+        }
+        if (ifans)
+        {
+            count++;
+            ans.push_back(testnum);
+        }
+    }
+    for (auto num : ans)
+        cout << num << " ";
+}
+
+int tmp;
+int tmpnum;
+int mynum;
+
+int hammingd(int aa, int bb) //待核查
+{
+    mynum = 0;
+    tmpnum = aa ^ bb;
+    //i=0单独判断
+    if (tmpnum % 2 == 1)
+        mynum = 1;
+    //最后结果总是多一是因为这里没有除以二，其实不用特殊处理
+    tmpnum /= 2;
+    for (int i = 1; i < b; i++)
+    {
+        if (tmpnum % 2 == 1)
+        {
+            mynum++;
+        }
+        tmpnum = tmpnum / 2;
+    }
+    return mynum; //要减一？ 无论如何都是4 测试出问题了
+    //7和25输出也是4？
+    // return 0;
 }
