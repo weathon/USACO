6200da5e15bce9c4c107c768f2345c7fe01bc213
weathon <wguo6358@gmail.com>
2020-03-23 16:51:33 -0500

    11111

--- luogu/P5836.cpp
+++ luogu/P5836.cpp
@@ -37,31 +37,70 @@ int main(void)
     {
         cin >> A[i] >> B[i] >> which_kind_of_cows_i_like[i];
     }
-
+    bool ans;
+    int tmp3, tmp4;
     for (int i = 0; i < M; i++)
     {
         vector<int> astring;
         vector<int> bstring;
         int pointer = A[i];
-        // astring.push_back(pointer);
-        // while(A[pointer]!=1)
         while (pointer != 1)
         {
             astring.push_back(pointer); //分不清A和mytree
             pointer = mytree[pointer];  //把pointer携程A[i] 警告
         }
         astring.push_back(1);
-        // for(auto j:astring) cout<<j<<endl;
-        // cout<<endl;
-        int pointer = A[i];
-        // astring.push_back(pointer);
-        // while(A[pointer]!=1)
+        pointer = B[i];
         while (pointer != 1)
         {
-            astring.push_back(pointer); //分不清A和mytree
+            bstring.push_back(pointer); //分不清A和mytree
             pointer = mytree[pointer];  //把pointer携程A[i] 警告
         }
-        astring.push_back(1);
+        bstring.push_back(1);
+        //从前往后扫描，扫到一样的就停止，然后
+        //Len of astring and bstring maight not be the same
+        ans = false;
+        int index = 0;
+        int equalindex;
+        for (int i = 0; true; i++)
+        {
+            if (astring[astring.size() - i] != bstring[bstring.size() - i])
+            {
+                equalindex = astring[astring.size() - i + 1];
+                break;
+            }
+        }
+
+        while (1)
+        {
+            tmp3 = astring[index];
+            if (which_kind_of_cows_farm_i_has[tmp3] == which_kind_of_cows_i_like[i])
+            {
+                ans = true;
+                break;
+            }
+            if (tmp3 == equalindex)
+            {
+                break;
+            }
+            index++;
+        }
+        index=0;
+        while (1)
+        {
+            tmp4 = bstring[index];
+            if (which_kind_of_cows_farm_i_has[tmp4] == which_kind_of_cows_i_like[i])
+            {
+                ans = true;
+                break;
+            }
+            if (tmp4 == equalindex)
+            {
+                break;
+            }
+            index++;
+        }
+        printf("%d", ans);
     }
     return 0;
 }
