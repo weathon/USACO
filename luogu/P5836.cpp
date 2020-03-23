6322316098b21736cb6c9ac4cc86d9f29986a62b
weathon <wguo6358@gmail.com>
2020-03-23 17:33:32 -0500

    神奇！a在里面，b在外面就没事？？？？？？？？？？？？？

--- luogu/P5836.cpp
+++ luogu/P5836.cpp
@@ -18,16 +18,17 @@ int A[100005];
 int B[100005];
 int mytree[100005];
 int tmp1, tmp2;
+// vector<int> asatring;
 
-int main(void)
+int main()
 {
     cin >> N >> M;
-    getchar();
-    for (int i = 0; i < N; i++)
+    // getchar();
+    for (int i = 1; i <= N; i++) //还是弄错
     {
-        which_kind_of_cows_farm_i_has[i] = getchar();
+        cin >> which_kind_of_cows_farm_i_has[i];
     }
-    getchar();
+    // getchar();
     for (int i = 0; i < N - 1; i++)
     {
         cin >> tmp1 >> tmp2;
@@ -39,10 +40,25 @@ int main(void)
     }
     bool ans;
     int tmp3, tmp4;
+
+    vector <int> bstring;//a在里面，b在外面就没事？？？？？？？？？？？？？
     for (int i = 0; i < M; i++)
     {
-        vector<int> astring;
-        vector<int> bstring;
+    vector <int> astring; //为什么这个放在外面就RE?
+
+        astring.clear();
+        bstring.clear();
+        if (A[i] == B[i])
+        {
+            if (which_kind_of_cows_farm_i_has[A[i]] == which_kind_of_cows_i_like[i])
+                putchar('1');
+            else
+            {
+                putchar('0');
+            }
+            continue;
+        }
+
         int pointer = A[i];
         while (pointer != 1)
         {
@@ -57,48 +73,49 @@ int main(void)
             pointer = mytree[pointer];  //把pointer携程A[i] 警告
         }
         bstring.push_back(1);
+
         //从前往后扫描，扫到一样的就停止，然后
         //Len of astring and bstring maight not be the same
         ans = false;
-        int index = 0;
-        int equalindex;
-        for (int i = 0; true; i++)
+        int myindex = 0;
+        int equalmyindex;
+        for (int i = 0; i < min(bstring.size(), astring.size()); i++) //没考虑完全一样的情况
         {
             if (astring[astring.size() - i] != bstring[bstring.size() - i])
             {
-                equalindex = astring[astring.size() - i + 1];
+                equalmyindex = astring[astring.size() - i + 1];
                 break;
             }
         }
 
         while (1)
         {
-            tmp3 = astring[index];
+            tmp3 = astring[myindex]; //似乎是这里溢出？
             if (which_kind_of_cows_farm_i_has[tmp3] == which_kind_of_cows_i_like[i])
             {
                 ans = true;
                 break;
             }
-            if (tmp3 == equalindex)
+            if (which_kind_of_cows_farm_i_has[tmp3] == equalmyindex)
             {
                 break;
             }
-            index++;
+            myindex++;
         }
-        index=0;
+        myindex = 0;
         while (1)
         {
-            tmp4 = bstring[index];
+            tmp4 = bstring[myindex];
             if (which_kind_of_cows_farm_i_has[tmp4] == which_kind_of_cows_i_like[i])
             {
                 ans = true;
                 break;
             }
-            if (tmp4 == equalindex)
+            if (which_kind_of_cows_farm_i_has[tmp4] == equalmyindex) //还是经常分不清索引和数值
             {
                 break;
             }
-            index++;
+            myindex++;
         }
         printf("%d", ans);
     }
